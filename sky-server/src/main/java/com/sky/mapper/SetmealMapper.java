3951ec20be0ec8883dd3b15b99b8d6fd6e474df2
waxgourd <waxgourdzs@gmail.com>
2026-04-13 12:09:23 +0800

    分页查询套餐代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -28,4 +31,8 @@ public interface SetmealMapper {
     * */
     @AutoFill(OperationType.INSERT)
     void insert(Setmeal setmeal);
+    /*
+    * 分页查询
+    * */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,10 +1,16 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
     /*
     * 新增套餐
     * */
     void save(SetmealDTO setmealDTO);
+    /*
+    * 分页查询
+    * */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
